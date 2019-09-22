f19ff758954249531220b652844d85b46337ea90
noel <noelgaur@analyticservice.net>
2019-09-22 08:08:03 +0800

    created setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rasa_aas',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['rasa_aas'],
+      zip_safe=False)
